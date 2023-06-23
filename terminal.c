ad0d5379c7491c25070548de5ef6b463fc00fb74
ItsVitor <79506366+ItsVitor@users.noreply.github.com>
2023-06-23 12:35:26 -0300

    Refactor + comentários

--- terminal.c
+++ terminal.c
@@ -1,59 +1,86 @@
 #include "terminal.h"
 
+#define MAX_PROGRAMAS_LINHA 5
+#define MAX_ARGUMENTOS_PROGRAMA 3
 
+char* acsh_read_line() {
+    char * entrada = NULL;
+    size_t size = 0;
+    getline(&entrada, &size, stdin);
+    // TODO: tratar falha de leitura, liberando memória.
+    /* "If *lineptr is set to NULL and *n is set 0 before the call, then getline() will al‐
+       locate a buffer for storing the line.  This buffer should be freed by the user pro‐
+       gram even if getline() failed." */
+    return entrada;
+}
 
-void RodaTerminal(){
-    char* entrada;
-    size_t size = 30;
-    int lidos;
-    entrada = (char*)malloc(size * sizeof(char));
-    while(1){
+void RodaTerminal()
+{
+    while (1)
+    {
         printf("acsh> ");
-        lidos = getline(&entrada, &size, stdin);
-        char* token;
-        if(!strcmp(entrada, "exit\n")){
+        char * entrada = acsh_read_line();
+        char *token;
+        if (!strcmp(entrada, "exit\n"))
+        {
+            // TODO: finalizar todos os processos de background que ainda estejam rodando.
             printf("\nsaindo\n");
             break;
         }
-        else if(!strcmp(entrada, "cd\n")){
+        else if (!strcmp(entrada, "cd\n"))
+        {
             printf("\nmudando diretorio\n");
         }
 
-        else if((token = strtok(entrada, " ")) != NULL){
+        else if ((token = strtok(entrada, " ")) != NULL)
+        {
             int i = 0;
-            char*** comandos = (char***)malloc(5 * sizeof(char**));
-            while(i < 5){
+            char ***comandos = (char ***)malloc(5 * sizeof(char **));
+            while (i < MAX_PROGRAMAS_LINHA)
+            {
                 int tam = 10;
-                comandos[i] = (char**)malloc(tam * sizeof(char*));          
+                comandos[i] = (char **)malloc(tam * sizeof(char *));
                 int j = 0;
-                while(token != NULL && strcmp(token, "<3") != 0){
-                    if(j >= tam){
+                while (token != NULL && strcmp(token, "<3") != 0)
+                {
+                    if (j >= tam)
+                    {
                         tam *= 2;
-                        comandos[i] = (char**)realloc(comandos[i], tam * sizeof(char*));
+                        comandos[i] = (char **)realloc(comandos[i], tam * sizeof(char *));
                     }
-                    comandos[i][j] = (char*)malloc(20 * sizeof(char));
+                    comandos[i][j] = (char *)malloc(20 * sizeof(char));
                     comandos[i][j] = strcpy(comandos[i][j], token);
+                    // penso que as duas linhas acima poderiam ser substituídas por:
+                    // comandos[i][j] = token;
+                    // token já é uma string alocada dinamicamente. não precisamos alocar
+                    // outra string e copiar o conteúdo de token para ela.
                     j++;
                     token = strtok(NULL, " ");
                 }
                 comandos[i][j] = NULL;
                 i++;
-                if(token == NULL ) break;
-                else token = strtok(NULL, " ");
+                if (token == NULL)
+                    break;
+                else
+                    token = strtok(NULL, " ");
             }
-            if(i > 5) printf("\nnumero invalido de comandos\n");
+            if (i > MAX_PROGRAMAS_LINHA)
+                printf("\nnumero invalido de comandos\n");
 
-            else if(i == 1){
+            else if (i == 1)
+            {
                 printf("\ncomando sozinho:\n");
                 int j = 0;
-                while(comandos[0][j] != NULL){
+                while (comandos[0][j] != NULL)
+                {
                     printf("%s ", comandos[0][j]);
                     j++;
                 }
                 printf("\n");
             }
 
-            else{
+            else
+            {
                 // printf("\ncomandos agrupados:\n");
                 // for(int k = 0;k < i;k++){
                 //     printf("comando %d:\n", (k + 1));
@@ -69,21 +96,23 @@ void RodaTerminal(){
     }
 }
 
-void ExecutaComandosExternos(char*** comandos, int nComandos){
-    for(int i = 0;i < nComandos;i++){
+void ExecutaComandosExternos(char ***comandos, int nComandos)
+{
+    for (int i = 0; i < nComandos; i++)
+    {
         int j = 0;
         int maiorComando = -1;
-        while (comandos[i][j] != NULL){
+        while (comandos[i][j] != NULL)
+        {
             j++;
-            if(strlen(comandos[i][j]) > maiorComando) maiorComando = strlen(comandos[i][j]);
+            if (strlen(comandos[i][j]) > maiorComando)
+                maiorComando = strlen(comandos[i][j]);
         }
         char comando[j][maiorComando];
-        for(int k = 0;k < j;k++){
-            //strcpy(comando[k], comandos[i][k]);
-            //printf("%s ", comando[k]);
+        for (int k = 0; k < j; k++)
+        {
+            // strcpy(comando[k], comandos[i][k]);
+            // printf("%s ", comando[k]);
         }
-
-        
-        
     }
 }
