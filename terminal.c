85742baa7ac83d751c57b5cea7da8d296d3713ac
ItsVitor <79506366+ItsVitor@users.noreply.github.com>
2023-06-23 21:11:10 -0300

    refactor (encapsulamento) + frees

--- terminal.c
+++ terminal.c
@@ -3,8 +3,9 @@
 #define MAX_PROGRAMAS_LINHA 5
 #define MAX_ARGUMENTOS_PROGRAMA 3
 
-char* acsh_read_line() {
-    char * entrada = NULL;
+char *acsh_read_line()
+{
+    char *entrada = NULL;
     size_t size = 0;
     getline(&entrada, &size, stdin);
     // TODO: tratar falha de leitura, liberando memória.
@@ -14,33 +15,109 @@ char* acsh_read_line() {
     return entrada;
 }
 
-void acsh_exec(char ** args) {
+char ***acsh_comandos_from_line(char *entrada)
+{
+    int i = 0;
+    char ***comandos = (char ***)malloc(MAX_PROGRAMAS_LINHA * sizeof(char **));
+    char *token = strtok(entrada, " ");
+    // printf("first token: %s\n", token);
+    while (i < MAX_PROGRAMAS_LINHA)
+    {
+        comandos[i] = (char **)malloc((MAX_ARGUMENTOS_PROGRAMA +
+                                       1 /* Próprio programa */ +
+                                       1 /* NULL no final */) * sizeof(char *));
+        int j = 0;
+        while (token != NULL && strcmp(token, "<3") != 0)
+        {
+            comandos[i][j] = token;
+            // printf("debug: [%d][%d] %s\n", i, j, comandos[i][j]);
+            j++;
+            if (j >= MAX_ARGUMENTOS_PROGRAMA + 2) {
+                printf("\nnumero invalido de argumentos\n");
+            }
+            token = strtok(NULL, " ");
+            // printf("new token read: %s\n", token);
+        }
+        comandos[i][j] = NULL; // Sinaliza o fim da lista de argumentos do programa.
+        i++;
+        // printf("debug i: %d\n", i);
+        if (token == NULL) { // Não tem mais programas para serem executados.
+            // Preenche o restante dos "ponteiros para programas" com NULL.
+            // Assim, é possível consultar depois quantos programas temos.
+            for (int k = i; k < MAX_PROGRAMAS_LINHA; k++) {
+                comandos[k] = NULL;
+            }
+            break;
+        }
+        else {
+            token = strtok(NULL, " ");
+            // printf("new token read: %s\n", token);
+        }
+    }
+    // printf("debug i: %d\n", i);
+    if (i >= MAX_PROGRAMAS_LINHA) {
+        printf("\nnumero invalido de comandos\n");
+    }
+
+    return comandos;
+}
+
+int qtd_comandos(char *** comandos) {
+    int i;
+    for (i = 0; i < MAX_PROGRAMAS_LINHA; i++) {
+        if (comandos[i] == NULL) return i;
+    }
+    return i;
+}
+
+void libera_comandos(char *** comandos) {
+    for (int i = 0; i < 1; i++) { 
+        // printf("debug liberando comandos: %d\n", i);
+        free(comandos[i]);
+    }
+    free(comandos);
+}
+
+void acsh_exec(char **args)
+{
     pid_t child_pid = fork();
 
     // Execução em foreground
-    if (child_pid == 0) {
+    if (child_pid == 0)
+    {
         execvp(args[0], args);
         perror("acsh");
         exit(1);
-    } else if (child_pid > 0) {
+    }
+    else if (child_pid > 0)
+    {
         int status;
-        do {
+        do
+        {
             waitpid(child_pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    } else {
+    }
+    else
+    {
         perror("acsh");
     }
 }
 
-void acsh_exit(char **args) {
+void acsh_exit(char **args)
+{
     exit(0);
 }
 
-void acsh_cd(char **args) {
-    if (args[1] == NULL) {
+void acsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
         printf("acsh: cd: missing argument\n");
-    } else {
-        if (chdir(args[1]) != 0) {
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
             perror("acsh: cd");
         }
     }
@@ -52,7 +129,8 @@ void RodaTerminal()
     {
         printf("acsh> ");
         char * entrada = acsh_read_line();
-        char *token;
+        char * entrada_original_ptr = entrada;
+        char ***comandos = acsh_comandos_from_line(entrada);
         if (!strcmp(entrada, "exit\n"))
         {
             // TODO: finalizar todos os processos de background que ainda estejam rodando.
@@ -64,55 +142,34 @@ void RodaTerminal()
             printf("\nmudando diretorio\n");
         }
 
-        else if ((token = strtok(entrada, " ")) != NULL)
-        {
-            int i = 0;
-            char ***comandos = (char ***)malloc(MAX_PROGRAMAS_LINHA * sizeof(char **));
-            while (i < MAX_PROGRAMAS_LINHA)
-            {
-                comandos[i] = (char **)malloc((MAX_ARGUMENTOS_PROGRAMA + 2) * sizeof(char *));
-                int j = 0;
-                while (token != NULL && strcmp(token, "<3") != 0)
-                {
-                    comandos[i][j] = (char *)malloc(20 * sizeof(char));
-                    comandos[i][j] = strcpy(comandos[i][j], token);
-                    // penso que as duas linhas acima poderiam ser substituídas por:
-                    // comandos[i][j] = token;
-                    // token já é uma string alocada dinamicamente. não precisamos alocar
-                    // outra string e copiar o conteúdo de token para ela.
-                    j++;
-                    token = strtok(NULL, " ");
-                }
-                comandos[i][j] = NULL;
-                i++;
-                if (token == NULL)
-                    break;
-                else
-                    token = strtok(NULL, " ");
-            }
-            if (i > MAX_PROGRAMAS_LINHA)
-                printf("\nnumero invalido de comandos\n");
+        printf("qtd comandos: %d\n", qtd_comandos(comandos));
 
+        ExecutaComandosExternos(comandos, qtd_comandos(comandos));
 
-            else{
-                ExecutaComandosExternos(comandos, i);
-            }
-        }
+        free(entrada);
+        libera_comandos(comandos);
     }
 }
 
-void ExecutaComandosExternos(char ***comandos, int nComandos){
-    int foreground = 0;   
-    if(nComandos == 1){
-        for(int i = 0;i < 5;i++){
-            if(comandos[0][i] == NULL) break;
-            if(comandos[0][i] == "%%"){
-                 pid_t pid = fork();
-                 foreground = 1;
-                 if(pid < 0){
+void ExecutaComandosExternos(char ***comandos, int nComandos)
+{
+    int foreground = 0;
+    if (nComandos == 1)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (comandos[0][i] == NULL)
+                break;
+            if (comandos[0][i] == "%%")
+            {
+                pid_t pid = fork();
+                foreground = 1;
+                if (pid < 0)
+                {
                     printf("erro ao tentar criar filho.");
-                 }
-                 if(pid == 0){
+                }
+                if (pid == 0)
+                {
                     execvp(comandos[0][0], comandos[0]);
                     printf("erro ao executar programa");
                     exit(0);
@@ -123,41 +180,49 @@ void ExecutaComandosExternos(char ***comandos, int nComandos){
                         argv[1] = "-la";
                         argv[2] = NULL;
                         execvp(cmd, argv); //This will run "ls -la" as if it were a command*/
-                 }
-                 if(pid > 0 ){
+                }
+                if (pid > 0)
+                {
                     int status;
-                    if(waitpid(pid, &status, 0) == -1){
+                    if (waitpid(pid, &status, 0) == -1)
+                    {
                         printf("erro ao aguardar por termino do filho");
                     }
-                 }
+                }
             }
         }
     }
-    if(foreground == 0){
-            pid_t pid = fork();
-            if(pid < 0){
-                printf("erro ao tentar criar filho.");
+    if (foreground == 0)
+    {
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            printf("erro ao tentar criar filho.");
+        }
+        if (pid == 0)
+        {
+            if (setsid() == -1)
+            {
+                printf("erro ao tentar colocar filho em bg");
             }
-            if(pid == 0){
-                if(setsid() == -1){
-                    printf("erro ao tentar colocar filho em bg");
+            for (int i = 1; i < nComandos; i++)
+            {
+                pid = fork();
+                if (pid < 0)
+                {
+                    printf("erro ao tentar criar filho.");
                 }
-                for(int i = 1;i < nComandos;i++){
-                    pid = fork();
-                    if(pid < 0){
-                        printf("erro ao tentar criar filho.");
-                    }
-                    if(pid == 0){
-                        execvp(comandos[i][0], comandos[i]);
-                        printf("erro ao executar programa");
-                        exit(0);
-                    }
+                if (pid == 0)
+                {
+                    execvp(comandos[i][0], comandos[i]);
+                    printf("erro ao executar programa");
+                    exit(0);
                 }
-                printf("%s", comandos[0][0]);
-                execvp(comandos[0][0], comandos[0]);
-                printf("erro ao executar programa");
-                exit(0);
             }
-
+            printf("%s", comandos[0][0]);
+            execvp(comandos[0][0], comandos[0]);
+            printf("erro ao executar programa");
+            exit(0);
+        }
     }
 }
